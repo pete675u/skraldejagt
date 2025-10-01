82d43bd9aabd1639c9710d6cfe4976b488ae9303
pete675u <104153586+pete675u@users.noreply.github.com>
2025-10-01 19:06:47 +0200

    Update code.js

--- code.js
+++ code.js
@@ -1,1 +1,18 @@
+fetch('data.json?' + new Date().getTime())
+console.log("FUCK THIS");
+function offSet() {
+//  setStyle("divApplab", "position", "relative");
+//  setStyle("divApplab", "top: 100vh");
+//  setStyle("divApplab", "left: 100vh");
+  setStyle("divApplab", "height: 100vh");
+  setStyle("divApplab", "width: 100vw");
+}
+function offSet2() {
+  setStyle("divApplab", "display", "flex");
+  setStyle("divApplab", "justify-content: center");
+  setStyle("divApplab", "align-items: center");
+  setStyle("divApplab", "text-align: center");
+  setStyle("divApplab", "width: 30%");
+}
 
+offSet2();
