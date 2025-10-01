7a75265cf004531440ae3891600ec67f359b60af
pete675u <104153586+pete675u@users.noreply.github.com>
2025-10-01 18:40:26 +0200

    Update code.js

--- code.js
+++ code.js
@@ -1,13 +1,1 @@
-const box = document.getElementById("divApplab");
 
-function resizeBox() {
-  const width = window.innerWidth;   // get window width
-  box.style.width = width + "px";    // set div width to match window
-  console.log(box);
-}
-
-// Run on page load
-resizeBox();
-
-// Update when the window is resized
-window.addEventListener("resize", resizeBox);
