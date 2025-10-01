1dab8c622d0fcce029125880cfaea4997177eb7f
pete675u <104153586+pete675u@users.noreply.github.com>
2025-10-01 18:35:57 +0200

    Update code.js

--- code.js
+++ code.js
@@ -2,11 +2,11 @@ function offSet2() {
 //  setStyle("divApplab", "position", "relative");
  // setStyle("divApplab", "top: 0px");
  // setStyle("divApplab", "left: 0px");
+  setStyle("divApplab", "margin: 0");
+  setStyle("divApplab", "padding: 0");
   setStyle("divApplab", "display", "flex");
   setStyle("divApplab", "justify-content: center");
   setStyle("divApplab", "align-items: center");
   setStyle("divApplab", "text-align: center");
-  setStyle("divApplab", "margin: 0");
-  setStyle("divApplab", "padding: 0");
 }
 offSet2();
