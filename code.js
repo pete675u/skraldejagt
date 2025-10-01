9975aff8e090cad85dc017bd3a2e5b1f6dcd2fba
pete675u <104153586+pete675u@users.noreply.github.com>
2025-10-01 18:39:26 +0200

    Update code.js

--- code.js
+++ code.js
@@ -3,6 +3,7 @@ const box = document.getElementById("divApplab");
 function resizeBox() {
   const width = window.innerWidth;   // get window width
   box.style.width = width + "px";    // set div width to match window
+  console.log(box);
 }
 
 // Run on page load
