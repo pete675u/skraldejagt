97c4bb5691c4b0b9a2c8cd8cdccc2a2a0a5226cf
pete675u <104153586+pete675u@users.noreply.github.com>
2025-10-03 15:49:04 +0200

    Update code.js

--- code.js
+++ code.js
@@ -1,8 +1,14 @@
 //Ctrl + Shift + R
+function resetdivAppLab() {
+  setStyle("divApplab", "width: 320px; height: 450px; display: block; left: 0px; top: 0px; position: absolute; z-index: 0; background-color: rgb(255, 255, 255);");
+  setStyle("divApplab", "transform: initial");
+  
+  
+
+}
+
 
 function offSet() {
-  setStyle("divApplab", "all: revert");
-  setStyle("divApplab", "display: block");
 //  setStyle("divApplab", "position", "relative");
 //  setStyle("divApplab", "top: 100vh");
 //  setStyle("divApplab", "left: 100vh");
@@ -15,13 +21,14 @@ function offSet() {
 //100 = 5
 //110 = 0
 //120 = -5
-  setStyle("divApplab", "top: -10%");
-  setStyle("divApplab", "left: -10%");
+  setStyle("divApplab", "top: 50%");
+  setStyle("divApplab", "left: 50%");
 //  setStyle("divApplab", "transform: scale(10)");
 //  setStyle("divApplab", "transform: scale(1.3)");
-  setStyle("divApplab", "transform: translate(-50%, -50%)");
-//  setStyle("divApplab", "transform: translate(-50%, -50%)");
-  setStyle("divApplab", "transform: scale(130%)");
+//setStyle("divApplab", "transform-origin: 0 0");
+  setStyle("divApplab", "transform: translate(-100%, -100%) scale(200%)");
+//  setStyle("divApplab", "transform: translate(-25%, -25%)");
+//  setStyle("divApplab", "transform: scale(50%)");
 //  setStyle("divApplab", "scale: calc(50vw / 1920)");
 
   
@@ -34,5 +41,5 @@ function offSet2() {
   setStyle("divApplab", "text-align: center");
   setStyle("divApplab", "width: 30%");
 }
-
+resetdivAppLab();
 offSet();
