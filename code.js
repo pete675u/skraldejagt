532e4ab42a6c6383d3b13204e959ebd104458ba6
pete675u <104153586+pete675u@users.noreply.github.com>
2025-10-03 15:01:55 +0200

    Update code.js

--- code.js
+++ code.js
@@ -15,7 +15,7 @@ function offSet() {
 //  setStyle("divApplab", "transform: scale(10)");
 //  setStyle("divApplab", "transform: translate(-50%, -50%)");
   setStyle("divApplab", "transform: translate(-50%, -50%)");
-  setStyle("divApplab", "transform: scale(2)");
+  setStyle("divApplab", "transform: scale(1.5)");
 
 
   
